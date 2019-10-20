725ae71026130848257d26793451b116add83887
amairovi <andrei.mairovich@electrolux.com>
2019-10-20 13:22:32 +0300

    Create login activity

---
+++ app/src/main/java/com/amairoiv/keeper/android/LoginActivity.kt
@@ -0,0 +1,38 @@
+package com.amairoiv.keeper.android
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.StrictMode
+import android.view.View
+import android.widget.EditText
+import android.widget.ListView
+import com.amairoiv.keeper.android.service.UserService
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val SDK_INT = android.os.Build.VERSION.SDK_INT
+                if (SDK_INT > 8) {
+                    val policy = StrictMode.ThreadPolicy.Builder()
+                        .permitAll().build()
+                    StrictMode.setThreadPolicy(policy)
+                }
+        setContentView(R.layout.activity_login)
+    }
+
+    fun authenticate(view: View) {
+        val userEmailEditText = findViewById<EditText>(R.id.userEmail)
+
+        val email = userEmailEditText.text.toString()
+
+        val userId = UserService.auth(email)
+
+        if (userId != null){
+            UserService.setUser(userId)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
