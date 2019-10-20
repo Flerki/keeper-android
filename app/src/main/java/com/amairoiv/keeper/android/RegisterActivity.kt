2e1c6fea50c4a0b23440dba4e0743031cffeba08
amairovi <andrei.mairovich@electrolux.com>
2019-10-20 21:30:55 +0300

    Implement activity for registration

---
+++ app/src/main/java/com/amairoiv/keeper/android/RegisterActivity.kt
@@ -0,0 +1,44 @@
+package com.amairoiv.keeper.android
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.StrictMode
+import android.view.View
+import android.widget.EditText
+import com.amairoiv.keeper.android.service.UserService
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        val SDK_INT = android.os.Build.VERSION.SDK_INT
+        if (SDK_INT > 8) {
+            val policy = StrictMode.ThreadPolicy.Builder()
+                .permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+        }
+        setContentView(R.layout.activity_register)
+    }
+
+    fun register(view: View) {
+        val userEmailEditText = findViewById<EditText>(R.id.registerEmail)
+
+        val email = userEmailEditText.text.toString()
+        val userId = UserService.register(email)
+
+        UserService.setUser(userId)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    fun showAuthenticationActivity(view: View) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+
+        finish()
+    }
+}
