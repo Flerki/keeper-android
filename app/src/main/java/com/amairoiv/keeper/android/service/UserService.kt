7dafe28ff5827338e1bf5619cc8d25988f5c049d
amairovi <andrei.mairovich@electrolux.com>
2019-10-20 13:22:08 +0300

    Create UserService

---
+++ app/src/main/java/com/amairoiv/keeper/android/service/UserService.kt
@@ -0,0 +1,37 @@
+package com.amairoiv.keeper.android.service
+
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+
+
+object UserService {
+
+    private val client = OkHttpClient()
+    private val JSON = "application/json; charset=utf-8".toMediaTypeOrNull()
+    private var userId: String? = null
+
+    fun auth(email: String): String? {
+        val json = "{\"email\":\"$email\"}"
+        val body = json.toRequestBody(JSON)
+        val url = "http://10.0.2.2:8080/users"
+        val request: Request = Request.Builder()
+            .url(url)
+            .post(body)
+            .build()
+        val response: Response = client.newCall(request).execute()
+        val result = response.body?.string()
+
+        return result
+    }
+
+    fun setUser(userId: String) {
+        this.userId = userId
+    }
+
+    fun getUser(): String {
+        return userId!!
+    }
+}
