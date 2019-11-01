44cc9d0edba481652dd36ab88fbaa2077a75e2a9
osipmd <osipmd@yandex.ru>
2019-11-01 23:54:55 +0300

    add confirmPassword field

--- app/src/main/java/com/amairoiv/keeper/android/RegisterActivity.kt
+++ app/src/main/java/com/amairoiv/keeper/android/RegisterActivity.kt
@@ -4,8 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.StrictMode
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import androidx.core.view.isVisible
 import com.amairoiv.keeper.android.service.UserService
 
 class RegisterActivity : AppCompatActivity() {
@@ -20,6 +25,26 @@ class RegisterActivity : AppCompatActivity() {
             StrictMode.setThreadPolicy(policy)
         }
         setContentView(R.layout.activity_register)
+        findViewById<TextView>(R.id.confirmUserPasswordError).isVisible = false
+        findViewById<EditText>(R.id.confirmUserPasswordRegister).addTextChangedListener(object :
+            TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                if (findViewById<EditText>(R.id.userPasswordRegister).text.toString() != p0.toString()) {
+                    findViewById<TextView>(R.id.confirmUserPasswordError).isVisible = true
+                    findViewById<Button>(R.id.registerButton).isClickable = false
+                } else {
+                    findViewById<TextView>(R.id.confirmUserPasswordError).isVisible = false
+                    findViewById<Button>(R.id.registerButton).isClickable = true
+                }
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                findViewById<TextView>(R.id.confirmUserPasswordError).isVisible = false
+            }
+        })
     }
 
     fun register(view: View) {
